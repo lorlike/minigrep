a17c7287283e583f3fb2445488516fa8a084ff99
lorlike <lorlike.me@gmail.com>
2026-06-06 18:39:46 +0800

    fix(main.rs): handle errors
    judge the length of args, handle the error of reading file

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
-    run(config);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename).unwrap();
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     for line in contents.lines() {
         if line.contains(&config.query) {
             println!("{line}");
         }
     }
+    Ok(())
 }
 
 struct Config {
@@ -22,9 +30,12 @@ struct Config {
     filename: String,
 }
 impl Config {
-    fn new(args: &[String]) -> Self {
+    fn new(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
