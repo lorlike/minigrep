c434510266b86fefc441e602f573a6a2844abfad
lorlike <lorlike.me@gmail.com>
2026-06-06 18:16:20 +0800

    feat(main.rs, poem.txt): add grep feature
    add grep feature and a file to test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            println!("{line}");
+        }
+    }
 }
