0055c70f1b20480a7011917518591d89fd5213ca
lorlike <lorlike.me@gmail.com>
2026-06-06 18:25:46 +0800

    feat(main.rs): pack args

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,24 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
-
-    let contents = fs::read_to_string(filename).unwrap();
+    let config = Config::new(&args);
+    let contents = fs::read_to_string(config.filename).unwrap();
 
     for line in contents.lines() {
-        if line.contains(query) {
+        if line.contains(&config.query) {
             println!("{line}");
         }
+    }
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Self {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
     }
 }
